0fce928e33cfc1818d856d9fd54c2b299c7ef3c2
aspectious <gdsiebert@outlook.com>
2025-08-28 11:54:25 -0500

    Added Warm-Ups and Merged with Master.

--- misc/8-27-25.py
+++ misc/8-27-25.py
@@ -1,8 +1,12 @@
 # Warmup
-num = input('Enter number: ')
-num = int(num)
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+#num = input('Enter number: ')
+#num = int(num)
+#if num % 2 == 0:
+#    print("Even")
+#else:
+#    print("Odd")
 
+x = 10
+while (x >= 0):
+    print(x)
+    x -= 2
