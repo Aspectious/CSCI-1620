052058e917e3a63f0506fa6a55157fd55e5109db
aspectious <gdsiebert@outlook.com>
2025-10-06 15:44:56 -0500

    Added Snippets for 10525

---
+++ snippets/10525.py
@@ -0,0 +1,18 @@
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fact(n-1)
+
+
+
+
+
+#print(fact(5))
+#mfw secure coding principles is tiring (why is it needed for every python script?)
+
+
+
+
+
+
