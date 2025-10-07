b71f7ee28d868fd61edc16a16dcdbe138cc6f5df
aspectious <gdsiebert@outlook.com>
2025-10-07 11:48:23 -0500

    Finalized Snippets from 10625

--- snippets/10525.py
+++ snippets/10525.py
@@ -8,6 +8,38 @@ def fact(n):
 
 
 
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def sum(n):
+    if n < 1:
+        return 1
+    else:
+        return n + sum(n-1)
+
+
+print(sum(666))
+
+
+
+
+
+
+
+
+
+
+
+#print(fib(5))
+
+
+
 #print(fact(5))
 #mfw secure coding principles is tiring (why is it needed for every python script?)
 
